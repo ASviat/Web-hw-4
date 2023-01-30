1252b97f2996ba75b498401dd2bcacc174ae9178
Sviatoslav <ecrprague.asi@gmail.com>
2023-01-30 18:27:34 +0300

    T2D

---
+++ my.sql
@@ -0,0 +1,22 @@
+-- create
+CREATE TABLE CLASSMATES (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  name TEXT NOT NULL,
+  age TEXT NOT NULL,
+  adress TEXT NOT NULL
+);
+
+-- insert
+INSERT INTO CLASSMATES (name, age, adress) VALUES ('Clark', '15', 'Minsk');
+INSERT INTO CLASSMATES (name, age, adress) VALUES ('Dave', '25', 'Moscow');
+INSERT INTO CLASSMATES (name, age, adress) VALUES ('Ava', '18', 'Rivno');
+INSERT INTO CLASSMATES (name, age, adress) VALUES ('Mark', '38', 'Riga');
+INSERT INTO CLASSMATES (name, age, adress) VALUES ('Vovan', '29', 'Warsaw');
+INSERT INTO CLASSMATES (name, age, adress) VALUES ('Dima', '35', 'Prague');
+INSERT INTO CLASSMATES (name, age, adress) VALUES ('Momo', '13', 'Moskow');
+INSERT INTO CLASSMATES (name, age, adress) VALUES ('Marina', '30', 'Moscow');
+INSERT INTO CLASSMATES (name, age, adress) VALUES ('Escobar', '44', 'WholeKolumbia');
+
+
+-- fetch 
+SELECT name FROM CLASSMATES WHERE adress='Moscow' AND age BETWEEN '18' AND '30';
